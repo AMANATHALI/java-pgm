8779d2d9aff8a6d208b6f14db6adc3ab3bad649c
AMANATHALI <34913632+AMANATHALI@users.noreply.github.com>
2018-03-14 16:24:27 +0530

    Add files via upload

---
+++ number2.java
@@ -0,0 +1,15 @@
+class number
+{
+public static void main(String args[])
+{
+int i,j;
+int n=1;
+for(i=1;i<=3;i++)
+{
+for(j=1;j<=i;j++)
+{
+System.out.print(n);
+n=n+1;
+}
+System.out.println();
+}}}
