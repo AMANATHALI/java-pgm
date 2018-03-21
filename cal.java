ff1bb9942b82051cbdfef94c2329d9c0a6f2ddb5
AMANATHALI <34913632+AMANATHALI@users.noreply.github.com>
2018-03-21 20:24:15 +0530

    Add files via upload

---
+++ cal.java
@@ -0,0 +1,49 @@
+import java.util.*;
+class call
+{
+public static void main(String arg[])
+{
+Scanner scan=new Scanner(System.in);
+float a,b;
+char operator;
+do
+{
+System.out.print("1. Addition");
+System.out.print("2. Subtraction");
+System.out.print("3. Multiplication");
+System.out.print("4. Division");
+System.out.println("5. Exit");
+System.out.println("Enter Your Choice : ");
+operator=scan.next().charAt(0);
+switch(operator)
+{
+case '1' : System.out.println("Enter Two Number : ");
+               a = scan.nextFloat();
+               b = scan.nextFloat();
+               System.out.print( a + b);
+               break;
+case '2' : System.out.println("Enter Two Number : ");
+                    a = scan.nextFloat();
+                    b = scan.nextFloat();
+                    System.out.print( a - b);
+                    break;
+ case '3' : System.out.println("Enter Two Number : ");
+                    a = scan.nextFloat();
+                    b = scan.nextFloat();
+                    System.out.print( a * b);
+                    break;
+ case '4' : System.out.println("Enter Two Number : ");
+                    a = scan.nextFloat();
+                    b = scan.nextFloat();
+                    System.out.print( a / b);
+                    break;
+ case '5' : System.exit(0);
+                    break;
+ default :
+                  System.out.print("Wrong Choice!!!");
+                  break;
+}
+}while(operator != 5);       
+}
+}
+ 
