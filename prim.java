99de645b908e99ebc1582be1d1284ebb91c9d645
AMANATHALI <34913632+AMANATHALI@users.noreply.github.com>
2018-03-22 08:35:40 +0530

    Add files via upload

---
+++ prim.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class prime
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+int a;
+int b=1;
+System.out.println("Enter the number");
+a=sc.nextInt(); 
+for(int i=2;i<a/i;i++)
+{
+if(a%i==0)
+{
+b=0;
+break;
+}
+}
+if(b==1)
+{
+System.out.println("the given number is prime");
+else
+{
+System.out.println("the given number is not prime");
+}}}}
